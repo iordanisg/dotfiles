0c9cf4219df025d460e9edcaa2859862c42ebaf0
Iordanis Grigoriou <iordanis.grigoriou@gmail.com>
2021-09-12 21:45:18 +0200

    Remove empty args maps

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -97,39 +97,21 @@ lvim.lsp.diagnostics.virtual_text = false
 
 -- set a formatter if you want to override the default lsp one (if it exists)
 lvim.lang.go.formatters = {
-  {
-    exe = 'goimports',
-    args = {},
-  },
-  {
-    exe = 'gofumpt',
-    args = {},
-  },
+  { exe = 'goimports' },
+  { exe = 'gofumpt' },
 }
 lvim.lang.python.formatters = {
-  {
-    exe = 'black',
-    args = {},
-  },
+  { exe = 'black' },
 }
 lvim.lang.lua.formatters = {
-  {
-    exe = 'stylua',
-    args = {},
-  },
+  { exe = 'stylua' },
 }
 lvim.lang.sh.formatters = {
-  {
-    exe = 'shfmt',
-    args = {},
-  },
+  { exe = 'shfmt' },
 }
 -- set an additional linter
 lvim.lang.python.linters = {
-  {
-    exe = 'flake8',
-    args = {},
-  },
+  { exe = 'flake8' },
 }
 
 -- Additional Plugins
