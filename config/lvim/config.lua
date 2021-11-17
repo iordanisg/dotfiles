8cf28fb885808215145a7fa3770338a3fd9294d2
Iordanis Grigoriou <iordanis.grigoriou@gmail.com>
2021-11-17 09:49:55 +0100

    Update lvim config

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -30,19 +30,34 @@ lvim.keys.normal_mode['<C-s>'] = ':w<cr>'
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
 --   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+--   i = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--     ["<C-n>"] = actions.cycle_history_next,
+--     ["<C-p>"] = actions.cycle_history_prev,
+--   },
 --   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
+--   n = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--   },
+-- }
 
 -- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+-- }
 lvim.builtin.which_key.mappings['G'] = { '<cmd>G<CR>', 'Fugitive' }
 lvim.builtin.which_key.mappings['P'] = { '<cmd>Telescope projects<CR>', 'Projects' }
 lvim.builtin.which_key.mappings['r'] = { '<cmd>RnvimrToggle<CR>', 'Ranger' }
@@ -66,19 +81,32 @@ lvim.builtin.which_key.mappings['sS'] = {
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.side = 'left'
+lvim.builtin.nvimtree.setup.view.side = 'left'
 lvim.builtin.nvimtree.show_icons.git = 1
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = 'maintained'
+
+lvim.builtin.treesitter.ignore_install = { 'haskell' }
+lvim.builtin.treesitter.highlight.enabled = true
 
 local components = require('lvim.core.lualine.components')
 lvim.builtin.lualine.sections.lualine_y = { components.location }
 lvim.builtin.lualine.sections.lualine_z = { components.scrollbar }
 
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = 'maintained'
-lvim.builtin.treesitter.ignore_install = { 'haskell' }
-lvim.builtin.treesitter.highlight.enabled = true
-
 -- generic LSP settings
+
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.automatic_servers_installation = false
+
+-- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
+-- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
+-- vim.list_extend(lvim.lsp.override, { "pyright" })
+
+-- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pylsp", opts)
+
 -- you can set a custom on_attach function that will be used for all the language servers
 -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
 -- lvim.lsp.on_attach_callback = function(client, bufnr)
@@ -86,7 +114,7 @@ lvim.builtin.treesitter.highlight.enabled = true
 --     vim.api.nvim_buf_set_option(bufnr, ...)
 --   end
 --   --Enable completion triggered by <c-x><c-o>
---   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 -- end
 -- you can overwrite the null_ls setup table (useful for setting the root_dir function)
 -- lvim.lsp.null_ls.setup = {
@@ -105,27 +133,50 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- end
 lvim.lsp.diagnostics.virtual_text = false
 
--- set a formatter if you want to override the default lsp one (if it exists)
-lvim.lang.go.formatters = {
-  { exe = 'goimports' },
-  { exe = 'gofumpt' },
-}
-lvim.lang.lua.formatters = {
-  { exe = 'stylua' },
-}
-lvim.lang.python.formatters = {
-  { exe = 'black' },
-}
-lvim.lang.sh.formatters = {
-  { exe = 'shfmt' },
-}
-lvim.lang.terraform.formatters = {
-  { exe = 'terraform_fmt' },
-}
--- set an additional linter
-lvim.lang.python.linters = {
-  { exe = 'flake8' },
-}
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { exe = "black", filetypes = { "python" } },
+--   { exe = "isort", filetypes = { "python" } },
+--   {
+--     exe = "prettier",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     args = { "--print-with", "100" },
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+local formatters = require('lvim.lsp.null-ls.formatters')
+formatters.setup({
+  { exe = 'black', filetypes = { 'python' } },
+  { exe = 'stylua', filetypes = { 'lua' } },
+  { exe = 'shfmt', filetypes = { 'sh' } },
+  { exe = 'terraform_fmt', filetypes = { 'terraform' } },
+  { exe = 'goimports', filetypes = { 'go' } },
+  { exe = 'gofumpt', filetypes = { 'go' } },
+})
+
+-- -- set additional linters
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { exe = "flake8", filetypes = { "python" } },
+--   {
+--     exe = "shellcheck",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     args = { "--severity", "warning" },
+--   },
+--   {
+--     exe = "codespell",
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "javascript", "python" },
+--   },
+-- }
+local linters = require('lvim.lsp.null-ls.linters')
+linters.setup({
+  { exe = 'flake8', filetypes = { 'python' } },
+})
 
 -- Additional Plugins
 lvim.plugins = {
